9da4f394197d1570b042517a19c5f7e4a473a9c7
moiz-xyz <a.moizniaxi123@gmail.com>
2025-03-11 22:41:34 +0500

    navbar component added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from "./Components/01_Navbar/Navbar.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/01_Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import "./navbar.css"
+import logo from "./download.png"; // Correct import
+function Navbar() {
+  return (
+    <div className="main">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="lists">
+        <li>Home</li>
+        <li>About</li>
+        <li>Support</li>
+      </div>
+      <div id="dowl">
+        <button>Download</button>
+
+      </div>
+    </div>
+  );
+}
+export  default Navbar
