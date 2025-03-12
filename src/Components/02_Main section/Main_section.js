12a16d791da1f57b2fb55e01973a2985dc320fda
moiz-xyz <a.moizniaxi123@gmail.com>
2025-03-12 15:11:33 +0500

    responsivness added of main section

--- src/Components/02_Main section/Main_section.js
+++ src/Components/02_Main section/Main_section.js
@@ -1,28 +1,31 @@
-import "./Main_section.css"
-import phone from "./image.png"
+import "./Main_section.css";
+import phone from "./image.png";
 
-function Main_section (){
-    return (
-        <div className="main2">
-            <div className="first-section">
-                <div className="head-para">
-                <h1>  AWESOME 
-                <span> APNEW </span> 
-                  MOBILE  APP.
-                </h1>
-                <p>
-                There are many variations of passages of Lorem Ipsum available, but the majorityhave suffered alteration in some form, by injected humour, or randomised words which don't look even There are many variations of passages of Lorem Ipsum available
-                </p>
-                </div>
-                <div className="button">
-                    <button> DOWNLOAD   </button>
-                    <button> LEARN MORE </button>
-                </div>
-            </div>
-            <div className="second-section">
-                <img src={phone} alt="phone"></img>
-            </div>
+function Main_section() {
+  return (
+    <div className="main2">
+      <div className="first-section">
+        <div className="head-para">
+          <h1>
+            AWESOME <span>APNEW</span> MOBILE APP.
+          </h1>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even. There are many
+            variations of passages of Lorem Ipsum available.
+          </p>
         </div>
-    )
+        <div className="button-container">
+          <button className="download-btn">DOWNLOAD</button>
+          <button className="learn-btn">LEARN MORE</button>
+        </div>
+      </div>
+      <div className="second-section">
+        <img src={phone} alt="phone" />
+      </div>
+    </div>
+  );
 }
-export default Main_section
+
+export default Main_section;
