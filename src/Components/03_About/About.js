664c8e7007a51e6e4f8e7d5a19b77343089d755d
moiz-xyz <a.moizniaxi123@gmail.com>
2025-03-12 22:18:52 +0500

    about edit

--- src/Components/03_About/About.js
+++ src/Components/03_About/About.js
@@ -1,4 +1,5 @@
-import "./About.css";  
+import "./About.css"; 
+import  mobile from "./mobile.png" 
 import iamge from "./image.png" ;
 let About = ()=> {
 
@@ -9,6 +10,19 @@ let About = ()=> {
                 <h2>ABOUT <span> APNEW </span> </h2>
                 <img src={iamge}></img>
                 <p>There are many variations of passages of Lorem Ipsum available, but the majorityhave suffered alteration in some form, by injected humour,available</p>
+            </div>
+            <div className="section">
+                <div className="mobile">
+                <img src={mobile}></img>
+                </div>
+                <div className="section-mini">
+                <h3>BEST <span> APNEW </span> </h3>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master of human happiness. No one rejects, dislikes, <br></br>
+                   mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the</p>               
+               <button> DOWNLOAD </button>
+               <button> LERAN MORE </button>
+                </div>
+
             </div>
         </div>
     )
